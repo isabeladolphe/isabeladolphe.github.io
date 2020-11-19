d4e056918cefc3024a53b4cac7a4201634cbe10a
isabeladolphe <70173852+isabeladolphe@users.noreply.github.com>
2020-11-19 15:33:34 -0500

    Add files via upload

---
+++ beerscript.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+	$(".stout1").mouseenter(function(){
+		$(".stouttext").addClass("showstouttext");
+	});
+	$(".stout1").mouseleave(function(){
+		$(".stouttext").removeClass("showstouttext");
+	})
+});
+
+$(document).ready(function(){
+	$(".lager1").mouseenter(function(){
+		$(".lagertext").addClass("showlagertext");
+	});
+	$(".lager1").mouseleave(function(){
+		$(".lagertext").removeClass("showlagertext");
+	})
+});
+
+$(document).ready(function(){
+	$(".ale1").mouseenter(function(){
+		$(".aletext").addClass("showaletext");
+	});
+	$(".ale1").mouseleave(function(){
+		$(".aletext").removeClass("showaletext");
+	})
+});
+
+$(document).ready(function(){
+	$(".ipa1").mouseenter(function(){
+		$(".ipatext").addClass("showipatext");
+	});
+	$(".ipa1").mouseleave(function(){
+		$(".ipatext").removeClass("showipatext");
+	})
+});
+
